d50820c1dc7f111edc702df66bd3bd8e58db9c22
Ashish Belwase <belwasetech@Ashishs-MacBook-Pro.local>
2019-01-02 11:57:26 +0545

    added electron

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
+
+declare const require: any;
+const { name, description, version } = require('../../package.json');
+
 export const environment = {
-  production: true
+  production: true,
+  api: 'http://192.168.1.87:8002/api',
+  name,
+  description,
+  version
 };
